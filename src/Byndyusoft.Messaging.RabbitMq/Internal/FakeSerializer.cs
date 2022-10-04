1e97181afef5d4e925c5c684910f3aa363ae1f0c
Yuriy Safonov <safonovyuv@byndyusoft.com>
2022-10-04 15:17:48 +0500

    added comment with reason for FakeSerializer existence

--- src/Byndyusoft.Messaging.RabbitMq/Internal/FakeSerializer.cs
+++ src/Byndyusoft.Messaging.RabbitMq/Internal/FakeSerializer.cs
@@ -4,6 +4,9 @@ using EasyNetQ;
 
 namespace Byndyusoft.Messaging.RabbitMq.Internal
 {
+    // fake implementation of ISerializer is required for removal of dependency from Newtonsoft.Json on user side
+    // default EasyNetQ implementation of ISerializer tries to get Newtonsoft.Json.JsonSerializer through reflection
+    // and throws exception if it finds none
     internal class FakeSerializer : ISerializer
     {
         const string MethodCallExceptionMessage = "ISerializer methods must not be called. Internal bug in Byndyusoft.Messaging.RabbitMq. Please, report it at https://github.com/Byndyusoft/Byndyusoft.Net.RabbitMq/issues";
