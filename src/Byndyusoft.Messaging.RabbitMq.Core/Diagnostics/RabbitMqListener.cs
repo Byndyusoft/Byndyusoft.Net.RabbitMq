188773b624aa58b620489d67ff75c65b14449ccb
Rena88 <foxcoolman@mail.ru>
2023-12-12 15:20:10 +0500

    Удалил EventItemBuilder

--- src/Byndyusoft.Messaging.RabbitMq.Core/Diagnostics/RabbitMqListener.cs
+++ src/Byndyusoft.Messaging.RabbitMq.Core/Diagnostics/RabbitMqListener.cs
@@ -2,8 +2,8 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Text;
 using Byndyusoft.Messaging.RabbitMq.Diagnostics.Base;
-using Byndyusoft.Messaging.RabbitMq.Diagnostics.Builders;
 using Byndyusoft.Messaging.RabbitMq.Diagnostics.Consts;
+using Byndyusoft.Messaging.RabbitMq.Serialization;
 using Byndyusoft.Messaging.RabbitMq.Settings;
 using Microsoft.Extensions.Logging;
 
@@ -11,6 +11,12 @@ namespace Byndyusoft.Messaging.RabbitMq.Diagnostics
 {
     public class RabbitMqListener : ListenerHandler
     {
+        private const string ExchangeDescription = "Exchange";
+        private const string RoutingKeyDescription = "RoutingKey";
+        private const string QueueDescription = "Queue";
+        private const string ContentDescription = "RoutingKey";
+        private const string PropertiesDescription = "RoutingKey";
+
         private readonly ILogger<RabbitMqListener> _logger;
         private readonly RabbitMqTracingOptions _options;
 
@@ -53,31 +59,98 @@ namespace Byndyusoft.Messaging.RabbitMq.Diagnostics
 
         private void OnMessagePublishing(Activity? activity, object? payload)
         {
-            var eventItems = EventItemBuilder.BuildFromMessagePublishing(payload, _options.DiagnosticsOptions);
+            var eventItems = BuildMessagePublishingEventItems(payload, _options.DiagnosticsOptions);
             Log(activity, eventItems, "message.publishing");
+        }
+
+        private EventItem[]? BuildMessagePublishingEventItems(object? payload, RabbitMqDiagnosticsOptions options)
+        {
+            if (payload is not RabbitMqMessage message)
+                return null;
+
+            var eventItems = new EventItem[]
+            {
+                new("amqp.message.exchange", message.Exchange ?? string.Empty, ExchangeDescription),
+                new("amqp.message.routing_key", message.RoutingKey, RoutingKeyDescription),
+                new("amqp.message.mandatory", message.Mandatory, "Mandatory"),
+                new("amqp.message.content", message.Content.ReadAsStringAsync().GetAwaiter().GetResult(), ContentDescription),
+                new("amqp.message.properties", JsonSerializer.Serialize(message.Properties, options), PropertiesDescription)
+            };
+
+            return eventItems;
         }
 
         private void OnMessageReturned(Activity? activity, object? payload)
         {
-            var eventItems = EventItemBuilder.BuildFromMessageReturned(payload, _options.DiagnosticsOptions);
+            var eventItems = BuildMessageReturnedEventItems(payload, _options.DiagnosticsOptions);
             Log(activity, eventItems, "message.returned");
+        }
+
+        private EventItem[]? BuildMessageReturnedEventItems(object? payload, RabbitMqDiagnosticsOptions options)
+        {
+            if (payload is not ReturnedRabbitMqMessage message)
+                return null;
+
+            var eventItems = new EventItem[]
+            {
+                new("amqp.message.exchange", message.Exchange ?? string.Empty, ExchangeDescription),
+                new("amqp.message.routing_key", message.RoutingKey, RoutingKeyDescription),
+                new("amqp.message.return_reason", message.ReturnReason, "ReturnReason"),
+                new("amqp.message.content", message.Content.ReadAsStringAsync().GetAwaiter().GetResult(),
+                    ContentDescription),
+                new("amqp.message.properties", JsonSerializer.Serialize(message.Properties, options),
+                    PropertiesDescription)
+            };
+
+            return eventItems;
         }
 
         private void OnMessageGot(Activity? activity, object? payload)
         {
-            var eventItems = EventItemBuilder.BuildFromMessageGot(payload, _options.DiagnosticsOptions);
+            var eventItems = BuildMessageConsumingEventItems(payload, _options.DiagnosticsOptions);
             Log(activity, eventItems, "message.got");
+        }
+
+        private EventItem[]? BuildMessageConsumingEventItems(object? payload, RabbitMqDiagnosticsOptions options)
+        {
+            if (payload is null)
+            {
+                return new EventItem[]
+                {
+                    new("amqp.message", null, "Message")
+                };
+            }
+
+            if (payload is not ReceivedRabbitMqMessage message)
+                return null;
+
+            var eventItems = new EventItem[]
+            {
+                new("amqp.message.content", message.Content.ReadAsStringAsync().GetAwaiter().GetResult(),
+                    ContentDescription),
+                new("amqp.message.exchange", message.Exchange, ExchangeDescription),
+                new("amqp.message.queue", message.Queue, QueueDescription),
+                new("amqp.message.routing_key", message.RoutingKey, RoutingKeyDescription),
+                new("amqp.message.delivery_tag", message.DeliveryTag, "DeliveryTag"),
+                new("amqp.message.redelivered", message.Redelivered, "Redelivered"),
+                new("amqp.message.consumer_tag", message.ConsumerTag, "ConsumerTag"),
+                new("amqp.message.retry_count", message.RetryCount, "RetryCount"),
+                new("amqp.message.properties", JsonSerializer.Serialize(message.Properties, options),
+                    PropertiesDescription)
+            };
+
+            return eventItems;
         }
 
         private void OnMessageReplied(Activity? activity, object? payload)
         {
-            var eventItems = EventItemBuilder.BuildFromMessageReplied(payload, _options.DiagnosticsOptions);
+            var eventItems = BuildMessageConsumingEventItems(payload, _options.DiagnosticsOptions);
             Log(activity, eventItems, "message.replied");
         }
 
         private void OnMessageConsumed(Activity? activity, object? payload)
         {
-            var eventItems = EventItemBuilder.BuildFromMessageConsumed(payload, _options.DiagnosticsOptions);
+            var eventItems = BuildMessageConsumedEventItems(payload);
             Log(activity, eventItems, "message.consumed");
         }
 
@@ -115,6 +188,19 @@ namespace Byndyusoft.Messaging.RabbitMq.Diagnostics
             }
 
             _logger.LogInformation(messageBuilder.ToString(), parameters.ToArray());
+        }
+
+        private EventItem[]? BuildMessageConsumedEventItems(object? payload)
+        {
+            if (payload is not ConsumeResult result)
+                return null;
+
+            var eventItems = new EventItem[]
+            {
+                new("result", result.GetDescription(), "Result")
+            };
+
+            return eventItems;
         }
     }
 }
